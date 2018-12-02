cfc7d467f5df83ce6017c3b69e67234fbad1a062
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-02 11:39:01 +0100

    Implement first advent day 1 puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    let frequency = input.lines().fold(0, |acc, line| acc + line.parse::<i32>().unwrap());
+    println!("{}", frequency);
 }
