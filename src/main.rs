877f53baf2781fb50987e32c4e0984753d265029
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-02 12:22:20 +0100

    Add second puzzle of day one

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("../input.txt");
 
-    let frequency = input.lines().fold(0, |acc, line| acc + line.parse::<i32>().unwrap());
+    let frequency = input
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .fold(0, |acc, value| acc + value);
     println!("{}", frequency);
+
+    let duplicate = input
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .cycle()
+        .scan(0, |state, value| { *state += value; Some(*state) })
+        .scan(HashSet::new(), |set, value| Some(set.replace(value)))
+        .find(|x|x.is_some())
+        .unwrap()
+        .unwrap();
+    println!("{:?}", duplicate);
 }
