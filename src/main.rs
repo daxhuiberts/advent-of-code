64c5687add47c0ef4dd093af562c2b030dc6c064
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-02 12:40:13 +0100

    Parse input only once

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,20 @@ use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../input.txt");
+    let parsed: Vec<i32> = input.lines().map(|line| line.parse().unwrap()).collect();
 
-    let frequency = input
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
+    let frequency = parsed
+        .iter()
         .fold(0, |acc, value| acc + value);
     println!("{}", frequency);
 
-    let duplicate = input
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
+    let duplicate = parsed
+        .iter()
         .cycle()
         .scan(0, |state, value| { *state += value; Some(*state) })
         .scan(HashSet::new(), |set, value| Some(set.replace(value)))
-        .find(|x|x.is_some())
-        .unwrap()
+        .filter_map(|value| value)
+        .next()
         .unwrap();
     println!("{:?}", duplicate);
 }
