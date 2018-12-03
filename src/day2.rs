2d204e8b0f89a3981bc181429c7592004fb3d1e4
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-03 21:05:22 +0100

    Simplify day 2 implementation a bit

--- src/day2.rs
+++ src/day2.rs
@@ -2,25 +2,21 @@ use super::IterExt;
 use super::itertools::Itertools;
 
 #[aoc(day2, part1)]
-pub fn part1(input: &str) -> i32 {
-    let (twos, threes) = input.lines().map(|entry| {
+pub fn part1(input: &str) -> usize {
+    let (twos, threes): (Vec<_>, Vec<_>) = input.lines().map(|entry| {
         let char_count = entry.chars().group_count();
         let count_count = char_count.values().group_count();
         (count_count.contains_key(&2), count_count.contains_key(&3))
-    }).fold((0, 0), |(mut twos, mut threes), (two, three)| {
-        if two { twos += 1 }
-        if three { threes += 1 }
-        (twos, threes)
-    });
+    }).unzip();
 
-    twos * threes
+    twos.iter().filter(|two| **two).count() * threes.iter().filter(|three| **three).count()
 }
 
 #[aoc(day2, part2)]
 pub fn part2(input: &str) -> String {
     input.lines().tuple_combinations().filter_map(|(a, b)| {
-        let (matching, differing): (Vec<_>, Vec<_>) = a.chars().zip(b.chars()).partition(|(aa, bb)| aa == bb);
-        if differing.len() == 1 {
+        let matching = a.chars().zip_eq(b.chars()).filter(|(aa, bb)| aa == bb).collect_vec();
+        if matching.len() == a.len() - 1 {
             Some(matching.iter().fold_ref(String::new(), |string, (aa, _bb)| string.push(*aa)))
         } else {
             None
