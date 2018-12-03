c6766e39f1b5142fc553a2edcf5cf93d4bcc69a0
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-03 17:15:50 +0100

    Implement parsing of input for day 3

---
+++ src/day3.rs
@@ -0,0 +1,41 @@
+use regex::Regex;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Claim {
+    id: usize,
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+}
+
+#[aoc_generator(day3)]
+pub fn parse_input(input: &str) -> Vec<Claim> {
+    let regex = Regex::new(r"\A#(\d)+ @ (\d),(\d): (\d)x(\d)\z").unwrap();
+
+    input.lines().map(|line| {
+        let captures = regex.captures(line).unwrap();
+        let values: Vec<_> = captures.iter().skip(1).map(|capture|
+            capture.unwrap().as_str().parse().unwrap()
+        ).collect();
+
+        Claim {
+            id: values[0],
+            x: values[1],
+            y: values[2],
+            width: values[3],
+            height: values[4],
+        }
+    }).collect()
+}
+
+#[test]
+fn test_parse_input() {
+    let input = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2";
+    let result = vec![
+        Claim { id: 1, x: 1, y: 3, width: 4, height: 4 },
+        Claim { id: 2, x: 3, y: 1, width: 4, height: 4 },
+        Claim { id: 3, x: 5, y: 5, width: 2, height: 2 },
+    ];
+    assert_eq!(parse_input(input), result);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
+extern crate itertools;
+extern crate regex;
+
 extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
-extern crate itertools;
+
+pub mod iterext;
 
 pub mod day1;
 pub mod day2;
-pub mod iterext;
+pub mod day3;
 
 use iterext::IterExt;
 
