e68a3136dc828ef7e00a719fc4a465883c8d9fc7
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-06 17:11:40 +0100

    Implement day 4

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ extern crate aoc_runner_derive;
 #[macro_use]
 extern crate aoctools;
 extern crate itertools;
+extern crate md5;
 
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
 
 aoc_lib!{ year = 2015 }
