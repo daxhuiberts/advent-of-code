eabb84fa3a78190a9c8b435796e66e28fff0253b
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-07 13:57:43 +0100

    Implement version of part 2 of day 7 which works with test data but not yet final data

--- src/day7.rs
+++ src/day7.rs
@@ -27,16 +27,61 @@ pub fn parse_input_step2(input: &[(char, char)]) -> HashMap<char, Vec<char>> {
 pub fn part1(prerequisites: &HashMap<char, Vec<char>>) -> String {
     let mut result = String::new();
 
-    while let Some(char) = get_next(prerequisites, &result) {
+    while let Some(char) = get_next(prerequisites, &result, "") {
         result.push(char);
     }
 
     result
 }
 
-fn get_next(prerequisites: &HashMap<char, Vec<char>>, done: &str) -> Option<char> {
+#[aoc(day7, part2)]
+pub fn part2(prerequisites: &HashMap<char, Vec<char>>) -> String {
+    part2_inner(prerequisites, 5, 60)
+}
+
+type Workers = Vec<Option<(char, usize)>>;
+
+fn part2_inner(prerequisites: &HashMap<char, Vec<char>>, workers: usize, delay: usize) -> String {
+    (0..).take(1200).fold((vec![None; workers], String::new()), |(workers, mut result): (Workers, String), _second| {
+        // println!("{}: {:?} -- {}", second, workers, result);
+
+        let workers2: Workers = workers.iter().map(|x| {
+            x.and_then(|(char, sec)| {
+                if sec == 1 {
+                    result.push(char);
+                    None
+                } else {
+                    Some((char, sec - 1))
+                }
+            })
+        }).collect_vec();
+
+        let exclude: String = workers2.iter().filter_map(|x| *x).map(|(char, _sec)| char).collect();
+        let new_workers = workers2.iter().scan(exclude, |exclude, worker| {
+            let res: Option<(char, usize)> = worker.or_else(||
+                get_next(prerequisites, &result, &exclude).map(|new_char| {
+                    exclude.push(new_char);
+                    (new_char, get_seconds(new_char) + delay)
+                })
+            );
+            Some(res)
+        }).collect_vec();
+
+        // println!("{}: {:?} -- {}", second, new_workers, result);
+
+        (new_workers, result)
+    }).1
+}
+
+fn get_seconds(char: char) -> usize {
+    char as usize - 64
+}
+
+fn get_next(prerequisites: &HashMap<char, Vec<char>>, done: &str, exclude: &str) -> Option<char> {
+    // println!("done: {}; exclude: {}", done, exclude);
     prerequisites.iter().filter(|(char, char_prerequisites)| {
-        !done.contains(**char) && char_prerequisites.iter().all(|dep_char| done.contains(*dep_char))
+        !done.contains(**char) && !exclude.contains(**char) &&
+            char_prerequisites.iter().all(|dep_char| done.contains(*dep_char))
     }).map(|x|*x.0).sorted().first().cloned()
 }
 
@@ -90,5 +135,16 @@ mod test {
     #[test]
     fn test_part1() {
         assert_eq!(part1(&*TEST_INPUT_RESULT), "CABDFE");
+    }
+
+    #[test]
+    fn test_part2_inner() {
+        assert_eq!(part2_inner(&*TEST_INPUT_RESULT, 2, 0), "CABFDE");
+    }
+
+    #[test]
+    fn test_get_seconds() {
+        assert_eq!(get_seconds('A'), 1);
+        assert_eq!(get_seconds('Z'), 26);
     }
 }
