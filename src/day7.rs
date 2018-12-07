aa461bcf9b39d04bff0ad1559e9cb11c06eed352
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-07 12:53:25 +0100

    Abstract input parsing a bit more to return a HashMap

--- src/day7.rs
+++ src/day7.rs
@@ -2,14 +2,17 @@ use std::collections::HashMap;
 use itertools::Itertools;
 
 #[aoc_generator(day7)]
-pub fn parse_input(input: &str) -> Vec<(char, char)> {
+pub fn parse_input(input: &str) -> Box<HashMap<char, Vec<char>>> {
+    Box::new(parse_input_step2(&parse_input_step1(input)))
+}
+
+pub fn parse_input_step1(input: &str) -> Vec<(char, char)> {
     input.lines().map(|line|
         line.chars().skip(1).filter(|char| char.is_ascii_uppercase()).collect_tuple().unwrap()
     ).collect()
 }
 
-#[aoc(day7, part1)]
-pub fn part1(input: &[(char, char)]) -> String {
+pub fn parse_input_step2(input: &[(char, char)]) -> HashMap<char, Vec<char>> {
     let mut prerequisites: HashMap<char, Vec<char>> = HashMap::new();
 
     for (before, after) in input {
@@ -17,6 +20,11 @@ pub fn part1(input: &[(char, char)]) -> String {
         prerequisites.entry(*after).or_default().push(*before);
     };
 
+    prerequisites
+}
+
+#[aoc(day7, part1)]
+pub fn part1(prerequisites: &HashMap<char, Vec<char>>) -> String {
     let mut result = String::new();
 
     while let Some(char) = prerequisites.iter().filter(|(char, char_prerequisites)| {
@@ -44,14 +52,35 @@ mod test {
     ");
 
     lazy_static! {
-        static ref TEST_INPUT_RESULT: Vec<(char, char)> = {
+        static ref TEST_INPUT_INTERMEDIATE_RESULT: Vec<(char, char)> = {
             vec![('C', 'A'), ('C', 'F'), ('A', 'B'), ('A', 'D'), ('B', 'E'), ('D', 'E'), ('F', 'E')]
+        };
+
+        static ref TEST_INPUT_RESULT: HashMap<char, Vec<char>> = {
+            let mut map = HashMap::new();
+            map.insert('A', vec!['C']);
+            map.insert('B', vec!['A']);
+            map.insert('C', vec![]);
+            map.insert('D', vec!['A']);
+            map.insert('E', vec!['B', 'D', 'F']);
+            map.insert('F', vec!['C']);
+            map
         };
     }
 
     #[test]
     fn test_parse_input() {
-        assert_eq!(parse_input(TEST_INPUT), *TEST_INPUT_RESULT);
+        assert_eq!(parse_input(TEST_INPUT), Box::new(TEST_INPUT_RESULT.clone()));
+    }
+
+    #[test]
+    fn test_parse_input_step1() {
+        assert_eq!(parse_input_step1(TEST_INPUT), *TEST_INPUT_INTERMEDIATE_RESULT);
+    }
+
+    #[test]
+    fn test_parse_input_step2() {
+        assert_eq!(parse_input_step2(&*TEST_INPUT_INTERMEDIATE_RESULT), *TEST_INPUT_RESULT);
     }
 
     #[test]
