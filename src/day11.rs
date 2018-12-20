5e496fb23532aa63d7ae6a27377ab04940533bb6
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-20 22:18:43 +0100

    Implement part 2 of day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,32 +1,53 @@
-const GRID_WIDTH: i32 = 300;
-const GRID_HEIGHT: i32 = 300;
+const GRID_SIZE: i32 = 300;
 
 #[aoc(day11, part1)]
 pub fn part1(input: &str) -> String {
     let serial = input.parse().unwrap();
     let grid = generate_grid(serial);
     let square_values = generate_square_values(&grid, 3);
+    let ((x, y), _value) = square_values
+        .into_iter()
+        .max_by_key(|(_, value)| *value)
+        .unwrap();
 
-    let position = square_values.iter().enumerate().max_by_key(|(_, total)| *total).unwrap().0;
-
-    format!("{},{}", (position as i32 % (GRID_WIDTH - 2)) + 1, (position as i32 / (GRID_WIDTH - 2)) + 1)
+    format!("{},{}", x, y)
 }
 
-fn generate_square_values(grid: &[i32], size: i32) -> Vec<i32> {
-    fn get(grid: &[i32], x: i32, y: i32) -> i32 {
-        grid[((y - 1) * GRID_WIDTH + (x - 1)) as usize]
-    }
+#[aoc(day11, part2)]
+pub fn part2(input: &str) -> String {
+    let serial = input.parse().unwrap();
+    let grid = generate_grid(serial);
 
+    let ((x, y), size, _) = (1..=20)
+        .map(|size| {
+            let square_values = generate_square_values(&grid, size);
+            let (position, value) = square_values
+                .into_iter()
+                .max_by_key(|(_, value)| *value)
+                .unwrap();
+            (position, size, value)
+        })
+        .max_by_key(|(_, _, value)| *value)
+        .unwrap();
+
+    format!("{},{},{}", x, y, size)
+}
+
+fn generate_square_values(grid: &[i32], size: i32) -> Vec<((i32, i32), i32)> {
     let mut square_values = vec![];
 
-    for y in 1..=(GRID_HEIGHT - size + 1) {
-        for x in 1..=(GRID_WIDTH - size + 1) {
+    for y_base in 1..=(GRID_SIZE - size + 1) {
+        for x_base in 1..=(GRID_SIZE - size + 1) {
+            let mut value = 0;
 
-            let value = get(grid, x, y) + get(grid, x + 1, y) + get(grid, x + 2, y) +
-                get(grid, x, y + 1) + get(grid, x + 1, y + 1) + get(grid, x + 2, y + 1) +
-                get(grid, x, y + 2) + get(grid, x + 1, y + 2) + get(grid, x + 2, y + 2);
+            for y_offset in 0..size {
+                for x_offset in 0..size {
+                    value += grid
+                        [((y_base + y_offset - 1) * GRID_SIZE + (x_base + x_offset - 1)) as usize];
+                }
+            }
 
-            square_values.push(value);
+            square_values.push(((x_base, y_base), value));
         }
     }
 
@@ -36,8 +57,8 @@ fn generate_square_values(grid: &[i32], size: i32) -> Vec<i32> {
 fn generate_grid(serial: i32) -> Vec<i32> {
     let mut grid = vec![];
 
-    for y in 1..=GRID_HEIGHT {
-        for x in 1..=GRID_WIDTH {
+    for y in 1..=GRID_SIZE {
+        for x in 1..=GRID_SIZE {
             grid.push(calculate_power_level(serial, x, y));
         }
     }
@@ -57,6 +78,12 @@ mod test {
     fn test_part1() {
         assert_eq!(part1("18"), "33,45");
         assert_eq!(part1("42"), "21,61");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("18"), "90,269,16");
+        assert_eq!(part2("42"), "232,251,12");
     }
 
     #[test]
