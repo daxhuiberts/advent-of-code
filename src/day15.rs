2711db9c9b93b5009de1944beaa7646cae635645
Dax Huiberts <daxhuiberts@gmail.com>
2019-10-20 14:32:53 +0200

    Add proper walking and make sure to only process each object once

--- src/day15.rs
+++ src/day15.rs
@@ -1,23 +1,102 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
 #[aoc(day15, part1)]
 pub fn part1(input: &str) -> usize {
     let mut grid = Grid::from_str(input);
-    step(&mut grid);
     println!("{:?}", grid);
+
+    for _ in 0..100 {
+        step(&mut grid);
+        println!("{:?}", grid);
+    }
 
     grid.width * grid.height
 }
 
 fn step(grid: &mut Grid) {
+    let mut visited = HashSet::new();
+
     for y in 0..grid.height {
         for x in 0..grid.width {
-            let cell = grid.get(x, y);
-            match cell {
-                '#' => (), // wall
-                '.' => (), // floor
-                'G' => (), // gnome
-                'E' => (), // Self
-                _ => panic!("not allowed"),
+            if !visited.contains(&(x, y)) {
+                let cell = grid.get(x, y);
+                match cell {
+                    '#' => (), // wall
+                    '.' => (), // floor
+                    'G' | 'E' => {
+                        if let Some((new_x, new_y)) = act(grid, x, y, cell) {
+                            visited.insert((new_x, new_y));
+                        }
+                    }
+                    _ => panic!("not allowed"),
+                }
             }
+        }
+    }
+
+    fn act(grid: &mut Grid, x: usize, y: usize, me: char) -> Option<(usize, usize)> {
+        if grid.get(x - 1, y) == opposite(me) ||
+            grid.get(x + 1, y) == opposite(me) ||
+            grid.get(x, y - 1) == opposite(me) ||
+            grid.get(x, y + 1) == opposite(me)
+        {
+            attack(grid, x, y, me);
+            None
+        } else {
+            walk(grid, x, y, me)
+        }
+    }
+
+    #[allow(unused_variables)]
+    fn attack(grid: &mut Grid, x: usize, y: usize, me: char) {
+
+    }
+
+    #[allow(unused_variables)]
+    fn walk(grid: &mut Grid, x: usize, y: usize, me: char) -> Option<(usize, usize)>{
+        let opposite = opposite(me);
+        let directions = [(0, usize::max_value()), (usize::max_value(), 0), (1, 0), (0, 1)];
+        let mut frontier = VecDeque::new();
+        let mut visited = HashSet::new();
+
+        for &(xoffset, yoffset) in &directions {
+            let cell = grid.gett(apply(x, xoffset, y, yoffset));
+            if cell == '.' {
+                frontier.push_back((apply(x, xoffset, y, yoffset), (xoffset, yoffset)));
+                visited.insert(apply(x, xoffset, y, yoffset));
+            }
+        }
+
+        while let Some(((xx, yy), direction)) = frontier.pop_front() {
+            for &(xoffset, yoffset) in &directions {
+                if !visited.contains(&apply(xx, xoffset, yy, yoffset)) {
+                    let cell = grid.gett(apply(xx, xoffset, yy, yoffset));
+                    if cell == opposite {
+                        let new_pos = apply(x, direction.0, y, direction.1);
+                        grid.set(x, y, '.');
+                        grid.sett(new_pos, me);
+                        return Some(new_pos);
+                    } else if cell == '.' {
+                        frontier.push_back((apply(xx, xoffset, yy, yoffset), direction));
+                        visited.insert(apply(xx, xoffset, yy, yoffset));
+                    }
+                }
+            }
+        }
+
+        return None;
+
+        fn apply(x: usize, xoffset: usize, y: usize, yoffset: usize) -> (usize, usize) {
+            (x.overflowing_add(xoffset).0, y.overflowing_add(yoffset).0)
+        }
+    }
+
+    fn opposite(me: char) -> char {
+        match me {
+            'G' => 'E',
+            'E' => 'G',
+            _ => panic!("not allowed")
         }
     }
 }
@@ -54,6 +133,27 @@ impl Grid {
 
         self.data[x + y * self.width]
     }
+
+    pub fn gett(&self, (x, y): (usize, usize)) -> char {
+        assert!(x < self.width, "x outside width");
+        assert!(y < self.height, "y outside height");
+
+        self.data[x + y * self.width]
+    }
+
+    pub fn set(&mut self, x: usize, y: usize, cell: char) {
+        assert!(x < self.width, "x outside width");
+        assert!(y < self.height, "y outside height");
+
+        self.data[x + y * self.width] = cell;
+    }
+
+    pub fn sett(&mut self, (x, y): (usize, usize), cell: char) {
+        assert!(x < self.width, "x outside width");
+        assert!(y < self.height, "y outside height");
+
+        self.data[x + y * self.width] = cell;
+    }
 }
 
 impl std::fmt::Debug for Grid {
@@ -74,7 +174,7 @@ mod test {
     use indoc::indoc;
 
     const ROUND_0: &'static str = indoc!(
-        r"
+        "
         #########
         #G..G..G#
         #.......#
@@ -88,7 +188,7 @@ mod test {
     );
 
     const ROUND_1: &'static str = indoc!(
-        r"
+        "
         #########
         #.G...G.#
         #...G...#
@@ -102,7 +202,7 @@ mod test {
     );
 
     const ROUND_2: &'static str = indoc!(
-        r"
+        "
         #########
         #..G.G..#
         #...G...#
@@ -116,7 +216,7 @@ mod test {
     );
 
     const ROUND_3: &'static str = indoc!(
-        r"
+        "
         #########
         #.......#
         #..GGG..#
@@ -132,13 +232,17 @@ mod test {
     #[test]
     fn test_grid_step() {
         let mut grid = Grid::from_str(ROUND_0);
+        println!("{:?}", grid);
 
         assert_eq!(format!("{:?}", grid), ROUND_0);
         step(&mut grid);
+        println!("{:?}", grid);
         assert_eq!(format!("{:?}", grid), ROUND_1);
         step(&mut grid);
+        println!("{:?}", grid);
         assert_eq!(format!("{:?}", grid), ROUND_2);
         step(&mut grid);
+        println!("{:?}", grid);
         assert_eq!(format!("{:?}", grid), ROUND_3);
     }
 }
