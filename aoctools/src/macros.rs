b0124b5787b03f196b98fb0d28d17198f4b687b7
Dax Huiberts <daxhuiberts@gmail.com>
2021-12-03 11:11:17 +0100

    Add transpose method to aoctools and organize library

---
+++ aoctools/src/macros.rs
@@ -0,0 +1,48 @@
+#[macro_export]
+macro_rules! main {
+    ( $day:literal ) => {
+        static INPUT: &str = include_str!(concat!("../../input/", $day, ".txt"));
+
+        fn main() {
+            println!("part 1: {}", part1(INPUT));
+            println!("part 2: {}", part2(INPUT));
+        }
+    };
+    ( $day:literal, parse_input ) => {
+        static INPUT: &str = include_str!(concat!("../../input/", $day, ".txt"));
+
+        fn main() {
+            let input = parse_input(INPUT);
+            println!("part 1: {}", part1(input.as_ref()));
+            println!("part 2: {}", part2(input.as_ref()));
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! mapper {
+    ( $( $l:literal => $r:literal ),* ) => {
+        |input| {
+            match input {
+                $(
+                    $l => $r,
+                )*
+                _ => panic!("failed to match"),
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! set {
+    ( $( $x:expr ),* ) => {
+        {
+            use std::collections::HashSet;
+            let mut temp_set = HashSet::new();
+            $(
+                temp_set.insert($x);
+            )*
+            temp_set
+        }
+    };
+}
